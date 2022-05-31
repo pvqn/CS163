d3bf4ae003bf5488c47f9324a882c99f7f60e67e
pvqn <phamvoquynhnhu24@gmail.com>
2022-05-31 14:26:51 +0700

    Add project files.

---
+++ Project_CS163/Project_CS163.cpp
@@ -0,0 +1,12 @@
+﻿// Project_CS163.cpp : Defines the entry point for the application.
+//
+
+#include "Project_CS163.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
