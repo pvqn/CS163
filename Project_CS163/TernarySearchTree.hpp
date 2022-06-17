41207f7bf6ca57623b6cdc5152ace7bbe14547a3
Hoang The Anh <101539204+htainvn@users.noreply.github.com>
2022-06-17 21:46:08 +0700

    Adding definition

--- Project_CS163/TernarySearchTree.hpp
+++ Project_CS163/TernarySearchTree.hpp
@@ -13,14 +13,15 @@ using namespace std;
 
 struct TreeNode {
     char data;
-    bool stringEnd;
+    string def;
+    
     TreeNode* left;
     TreeNode* mid;
     TreeNode* right;
     
-    TreeNode() : data(), stringEnd(false), left(nullptr), mid(nullptr), right(nullptr) {};
+    TreeNode() : data(), def(""), left(nullptr), mid(nullptr), right(nullptr) {};
     
-    TreeNode(char _data, bool _stringEnd) : data(_data), stringEnd(_stringEnd), left(nullptr), mid(nullptr), right(nullptr) {};
+    TreeNode(char& _data, string _def) : data(_data), def(_def), left(nullptr), mid(nullptr), right(nullptr) {};
     
 };
 
@@ -28,20 +29,20 @@ class TernarySearchTree {
 private:
     TreeNode* root;
     
-    TreeNode* insert(TreeNode* root, int index, string& s) {
+    TreeNode* insert(TreeNode* root, int index, string& s, string& def) {
         if (index == s.length()) return nullptr;
         
         if (!root) {
-            root = new TreeNode(s[index], (index == s.length()-1));
-            root->mid = insert(root->mid, index+1, s);
+            root = new TreeNode(s[index], ((index == s.length()-1) ? def : ""));
+            root->mid = insert(root->mid, index+1, s, def);
             return root;
         }
         
-        if (root->data == s[index]) root->mid = insert(root->mid, index+1, s);
+        if (root->data == s[index]) root->mid = insert(root->mid, index+1, s, def);
         
-        if (root->data > s[index]) root->left = insert(root->left, index, s);
+        if (root->data > s[index]) root->left = insert(root->left, index, s, def);
         
-        if (root->data < s[index]) root->right = insert(root->right, index, s);
+        if (root->data < s[index]) root->right = insert(root->right, index, s, def);
         
         return root;
     }
@@ -49,8 +50,8 @@ private:
 public:
     TernarySearchTree() : root(nullptr) {};
     
-    void insert(string key) {
-        root = insert(root, 0, key);
+    void insert(string key, string def) {
+        root = insert(root, 0, key, def);
     }
     
     /*void erase(string key) {
@@ -68,6 +69,7 @@ public:
     ~TernarySearchTree() {
         queue<TreeNode*> q;
         q.push(root);
+        
         while (!q.empty()) {
             TreeNode*& t = q.front(); q.pop();
             if (t->left) q.push(t->left);
