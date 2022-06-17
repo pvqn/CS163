551cb3fb0a3207549f6a89c442f9f89f1bb61149
pvqn <phamvoquynhnhu24@gmail.com>
2022-06-17 22:57:08 +0700

    Update TernarySearchTree.hpp

--- Project_CS163/TernarySearchTree.hpp
+++ Project_CS163/TernarySearchTree.hpp
@@ -9,44 +9,44 @@
 #include <queue>
 #include <string>
 #include <stdio.h>
-using namespace std;
+
 
 struct TreeNode {
     char data;
-    string def;
-    
+    std::string def;
+
     TreeNode* left;
     TreeNode* mid;
     TreeNode* right;
-    
+
     TreeNode() : data(), def(""), left(nullptr), mid(nullptr), right(nullptr) {};
-    
-    TreeNode(char& _data, string _def) : data(_data), def(_def), left(nullptr), mid(nullptr), right(nullptr) {};
-    
+
+    TreeNode(char& _data, std::string _def) : data(_data), def(_def), left(nullptr), mid(nullptr), right(nullptr) {};
+
 };
 
 class TernarySearchTree {
 private:
     TreeNode* root;
-    
-    TreeNode* insert(TreeNode* root, int index, string& s, string& def) {
+
+    TreeNode* insert(TreeNode* root, int index, std::string& s, std::string& def) {
         if (index == s.length()) return nullptr;
-        
+
         if (!root) {
-            root = new TreeNode(s[index], ((index == s.length()-1) ? def : ""));
-            root->mid = insert(root->mid, index+1, s, def);
+            root = new TreeNode(s[index], ((index == s.length() - 1) ? def : ""));
+            root->mid = insert(root->mid, index + 1, s, def);
             return root;
         }
-        
-        if (root->data == s[index]) root->mid = insert(root->mid, index+1, s, def);
-        
+
+        if (root->data == s[index]) root->mid = insert(root->mid, index + 1, s, def);
+
         if (root->data > s[index]) root->left = insert(root->left, index, s, def);
-        
+
         if (root->data < s[index]) root->right = insert(root->right, index, s, def);
-        
+
         return root;
     }
-    int deleteNode(TreeNode* &root, int index, string s)
+    int deleteNode(TreeNode*& root, int index, std::string s)
     {
         if (!root)
             return 0;
@@ -55,7 +55,7 @@ private:
             //if the string is in the tst
             if (!root->def.empty())
             {
-                return (root->left || root->right || root->mid);
+                return (!root->left && !root->right && !root->mid);
             }
             else return 0;
         }
@@ -66,44 +66,44 @@ private:
             else
                 if (s[index] < root->data)
                     return deleteNode(root->right, index, s);
-                else 
+                else
                     if (s[index] == root->data)
                     {
                         if (deleteNode(root->mid, index + 1, s))
                         {
                             delete root->mid;
                             root->mid = nullptr;
-                            // delete root if root dont have children
-                            return root->def.empty() && (root->left || root->mid || root->right);
+                            // delete root if root doesnt have children
+                            return root->def.empty() && (!root->left && !root->mid && !root->right);
                         }
                     }
         }
         return 0;
     }
-    
+
 public:
     TernarySearchTree() : root(nullptr) {};
-    
-    void insert(string key, string def) {
+
+    void insert(std::string key, std::string def) {
         root = insert(root, 0, key, def);
     }
-    
-    void erase(string key) {
+
+    void erase(std::string key) {
         deleteNode(root, 0, key);
-     }
-     
-     /*TreeNode* search(string key) {
-         
-     }
-     
-     void update(string key, string def) {
-         
-     }**/
-    
+    }
+
+    /*TreeNode* search(string key) {
+
+    }
+
+    void update(string key, string def) {
+
+    }**/
+
     ~TernarySearchTree() {
-        queue<TreeNode*> q;
+        std::queue<TreeNode*> q;
         q.push(root);
-        
+
         while (!q.empty()) {
             TreeNode*& t = q.front(); q.pop();
             if (t->left) q.push(t->left);
