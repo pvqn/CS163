6d79a8cfc3a043670914eaa633a1d1bfea607322
Hoang The Anh <101539204+htainvn@users.noreply.github.com>
2022-06-28 22:32:21 +0700

    Update dictionary.hpp

--- Project_CS163/dictionary.hpp
+++ Project_CS163/dictionary.hpp
@@ -3,12 +3,42 @@
 
 #include <stdio.h>
 #include "TernarySearchTree.hpp"
+#include <fstream>
 
-struct Dictionary {
+class Dictionary {
+private:
     int size;
     
     TernarySearchTree data;
     
+    std::string pathCurrentDataset;
+    
+public:
+    Dictionary() {
+        size = 0;
+        pathCurrentDataset = "slang.txt"; //might change later
+        load(pathCurrentDataset);
+    }
+    
+    void load(std::string path) {
+        pathCurrentDataset = ((path.substr(0,4) == "ORG_") ? path.substr(4, path.length()-3) : path);
+        std::ifstream fin(path);
+        std::string line;
+        while (getline(fin, line)) {
+            unsigned long delim = line.find('`');
+            data.insert(line.substr(0, delim), line.substr(delim+1, line.length()-delim-1));
+            ++size;
+        }
+        fin.close();
+    }
+    
+    void reset() {
+        data.~TernarySearchTree();
+        size = 0;
+        load("ORG_" + pathCurrentDataset);
+    }
 };
+
+void actionOnFavList(string a, bool status);
 
 #endif /* dictionary_hpp */
