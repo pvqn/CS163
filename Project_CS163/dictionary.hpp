f9b64cf4e03d43922be3a9f126301e137fe8dcca
Hoang The Anh <101539204+htainvn@users.noreply.github.com>
2022-06-28 21:34:48 +0700

    Create dictionary.hpp

---
+++ Project_CS163/dictionary.hpp
@@ -0,0 +1,14 @@
+#ifndef dictionary_hpp
+#define dictionary_hpp
+
+#include <stdio.h>
+#include "TernarySearchTree.hpp"
+
+struct Dictionary {
+    int size;
+    
+    TernarySearchTree data;
+    
+};
+
+#endif /* dictionary_hpp */
