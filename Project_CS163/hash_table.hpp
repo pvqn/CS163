f3e440a0b8c2a0f3a603de3d8e13e0e9889a0512
pvqn <phamvoquynhnhu24@gmail.com>
2022-06-29 16:55:10 +0700

    update, random 4 words

--- Project_CS163/hash_table.hpp
+++ Project_CS163/hash_table.hpp
@@ -5,56 +5,33 @@
  * 
  */
 
+const int size_hash = 1009;
+const int hash_num = 1000000097;
 #ifndef HASH_TABLE_HPP_
 #define HASH_TABLE_HPP_
-#include <cstddef>
 #include <string>
 #include <vector>
 #include <algorithm>
-
 #include "TernarySearchTree.hpp"
 
 class Hash_Table
 {
 private:
+   
     struct Bucket
     {
-        std::string word;
+        std::string word;                                       // keyword not word
         int key_hash_2, key_len;
-        TreeNode* location = nullptr;
-        Bucket( std::string keyword, int H2, int Length, TreeNode *loc) 
-            : word(keyword), key_hash_2(H2), key_len( Length ), location( loc ) {}
-    
-        bool operator==(const Bucket& other) const
-        {
-            return (key_hash_2 == other.key_hash_2)
-                && (key_len == other.key_len)
-                && (get_word(location) == get_word(other.location))
-                && (word == other.word);
-        }
-
-        bool operator<(const Bucket& other) const
-        {
-            if (key_hash_2 != other.key_hash_2)
-                return key_hash_2 < other.key_hash_2;
-            else if (key_len != other.key_len)
-                return key_len < other.key_hash_2;
-            std::string word = get_word(location);
-            std::string other_word = get_word(other.location);
-            if (word != other_word)
-                return word < other_word;
-            return word < other.word;
-        }
+        TreeNode* location;                                     // address of the end of word
+        Bucket( std::string keyword, int H2, int Length, TreeNode *loc) : word(keyword), key_hash_2(H2), key_len( Length ), location( loc ) {};
     };
-
-    std::vector<std::vector<Bucket>> table;
-
+    std::vector<std::vector<Bucket>> table = std::vector<std::vector<Bucket>> (size_hash);
     int hashing_1(std::string s)
     {
         int t = 1;
 
         for (char &ch: s)
-            t = (t * 31 + (int)(ch - 'a') + 1) % 1009;
+            t = (t * 31 + (ch - 'a') + 1) % size_hash;
         
         return t;
     }
@@ -64,33 +41,37 @@ private:
         long long t = 1;
 
         for (char &ch: s)
-            t = (t * 311 + (long long)(ch - 'a') + 1) % 1000000097;
+            t = (t * 311 + (ch - 'a') + 1) % hash_num;
         
-        return (int)t;
+        return t;
+    }
+
+    bool compare(Bucket A, Bucket B)
+    {
+        if (A.key_hash_2 != B.key_hash_2) return A.key_hash_2 < B.key_hash_2;
+        if (A.key_len != B.key_len) return A.key_len < B.key_len;
+        return A.word < B.word;
     }
 
     int binary_search( std::vector<Bucket> v, std::string cmp, TreeNode *address )
     {
-        Bucket consider = Bucket(cmp, hashing_2(cmp), (int)cmp.size(), address);
-        size_t L = std::lower_bound(v.begin(), v.end(), consider) - v.begin();
-        size_t R = std::upper_bound(v.begin(), v.end(), consider) - v.begin();
-        for (size_t i = L; i < R; ++i) if (v[i].location == address) return (int)i;
+        Bucket consider = Bucket(cmp, hashing_2(cmp), cmp.size(), address);
+        int L = lower_bound(v.begin(), v.end(), consider, compare) - v.begin();
+        int R = upper_bound(v.begin(), v.end(), consider, compare) - v.begin();
+        for (int i = L; i < R; ++i) if (v[i].location == address) return i;
         return -1;
     }
 
 public:
     
-    Hash_Table() { table.resize(1009); }
+    
+    Hash_Table() = default;
 
     void add( std::string keyword, TreeNode *address )
     {
         int index = hashing_1(keyword);
-
-        if (binary_search(table[index], keyword, address) != -1)
-        {
-            table[index].push_back(Bucket(keyword, hashing_2(keyword), keyword.size(), address));
-            std::sort(table[index].begin(), table[index].end());
-        }
+        table[index].push_back(Bucket(keyword, hashing_2(keyword), keyword.size(), address));
+        std::sort(table[index].begin(), table[index].end(), compare);
     }
 
     void remove( std::string keyword, TreeNode *address )
@@ -109,6 +90,28 @@ public:
         }
     }
     ~Hash_Table() = default;
+    std::vector<TreeNode*> random4Word()
+    {
+        std::vector<TreeNode*> result;
+        srand(time(0));
+        while (result.size() != 4)
+        {
+            int index1 = rand() % size_hash;;
+            if (!table[index1].empty())
+            {
+                int index2 = rand() % table[index1].size();
+                bool check = false;
+                for (int i = 0; i < result.size(); ++i)
+                    if (table[index1][index2].location == result[i])
+                    {
+                        check = true;
+                        break;
+                    }
+                if (!check) result.push_back(table[index1][index2].location);
+            }
+        }
+        return result;
+    }
 };
 
 #endif /* HASH_TABLE_HPP_ */
--- Project_CS163/quynh_nhu.cpp
+++ Project_CS163/quynh_nhu.cpp
@@ -1,5 +1,6 @@
 
 #include "dictionary.hpp"
+
 void printOutHistory()
 {
 	std::ifstream fin;
@@ -10,4 +11,21 @@ void printOutHistory()
 		std::string t; fin >> t;
 		//std::cout << t << "\n";
 	}
+}
+std::string getWord(TreeNode* eow)
+{
+	std::string word="";
+	while (eow)
+	{
+		if (eow->parent->mid == eow) word.push_back(eow->data);
+		eow = eow->parent;
+	}
+	return word;
+}
+void randomWord4Def(std::vector<std::string> words)
+{
+	srand(time(0));
+	int index = rand() % 4;
+	// std::cout 4 definitions
+	// std::cout getword(word[index]
 }
