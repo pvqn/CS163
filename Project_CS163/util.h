dd3be56a6ef9d05fb9d71124e06980b21ba714f1
Phạm Võ Quỳnh Như <phamvoquynhnhu24@gmail.com>
2022-07-29 18:13:13 +0700

    add spilt and to_upper

--- Project_CS163/util.h
+++ Project_CS163/util.h
@@ -17,9 +17,50 @@ namespace util
 	}
 	namespace str
 	{
+		std::string to_upper(std::string input)
+		{
+			for (char& c : input)
+				c = toupper(c);
+			return input;
+		}
 		std::vector<std::string> split(std::string definition)
 		{
-			return {};
+			std::vector<std::string> words;
+
+			std::vector<char> stop = { ':', '\t', '\'', ' ',
+										',', '.', '?', '!',
+										'@', '#', '(', ')',
+										'|' , ';', '"', '`', '\'',
+										'!', '$', '%', '^', '&', '*',
+										'-', '_', '+', '=', '{', '}',
+										'\\', '<', '>' };
+
+			std::string temp;
+
+			for (size_t i = 0; i < definition.size(); i++)
+			{
+				bool check = true;
+
+				for (int j = 0; j < stop.size() && check; j++)
+					check = (definition[i] != stop[j]);
+
+				if (check)
+				{
+					temp.push_back(definition[i]);
+				}
+				else
+				{
+					if (!temp.empty())
+						words.push_back(to_upper(temp));
+					temp.clear();
+				}
+
+			}
+
+			if (!temp.empty())
+				words.push_back(util::str::to_upper(temp));
+
+			return words;
 		}
 	}
 }
